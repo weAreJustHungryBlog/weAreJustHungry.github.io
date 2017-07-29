ccda0e8980392d14ea1662d61163f1c0d857aa95
weAreJustHungryBlog <30505090+weAreJustHungryBlog@users.noreply.github.com>
2017-07-29 06:36:00 +0100

    fix links of header.html and footer.html by adding ../

--- javaScript/loadHeaderFooter.js
+++ javaScript/loadHeaderFooter.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	//load the header
-	$(".headerContent").load("header.html");
+	$(".headerContent").load("../header.html");
 
 	//load the footer
-	$(".footerContent").load("footer.html");
+	$(".footerContent").load("../footer.html");
 });
